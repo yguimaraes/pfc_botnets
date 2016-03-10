a7e3c167aeae4fb872933b0b5ba57ee2df41c81d
Rocha Lima <jonasrla@gmail.com>
2016-03-10 14:20:16 -0300

    implements process_dns_log

--- matrix_feature_creator.cpp
+++ matrix_feature_creator.cpp
@@ -5,15 +5,33 @@
 #include "types/raw_fields.h"
 #include <fstream>
 #include <list>
-
+#include <regex>
 
 using namespace std;
 
+raw_fields* filter_fields(string request_line){
+    // ");//
+    raw_fields *compiled_fields = new raw_fields();
+    regex request_line_re("(\\d+-\\w+-\\d+) (\\d+:\\d+:\\d+.?\\d*) queries: info: client (\\d+\\.\\d+\\.\\d+\\.\\d+)#(\\d+): query: ([^ ]+) IN (\\w+) \\+ \\((\\d+\\.\\d+\\.\\d+\\.\\d+)\\)");
+    smatch request_line_match; //= sregex_iterator(request_line.begin(), request_line.end(), request_line_re);
 
-list<raw_fields> process_dns_log(string file_name){
+    regex_search(request_line, request_line_match, request_line_re);
+
+    compiled_fields->date = request_line_match[1];
+    compiled_fields->time = request_line_match[2];
+    compiled_fields->client_ip = request_line_match[3];
+    compiled_fields->client_port = request_line_match[4];
+    compiled_fields->url = request_line_match[5];
+    compiled_fields->package_record_type = request_line_match[6];
+    compiled_fields->dns_server_ip = request_line_match[7];
+    
+    return compiled_fields;
+}
+
+list<raw_fields*> process_dns_log(string file_name){
     
     ifstream dns_log_file;
-    list<raw_fields> dns_log_list;
+    list<raw_fields*> dns_log_list;
     string request_line;
 
     dns_log_file.exceptions ( ifstream::failbit );
@@ -22,16 +40,17 @@ list<raw_fields> process_dns_log(string file_name){
         dns_log_file.open(file_name);
     
         while (getline(dns_log_file, request_line)){
-            // TODO create new raw_fields instance for each line and add to dns_log_list
-            cout << request_line << endl;
+            dns_log_list.push_back(filter_fields(request_line));
         }
-    
+
         dns_log_file.close();
     }
     
     catch(ifstream::failure e){
         cerr << "Exception opening/reading file" << endl;
     }
+
+    // cout << dns_log_list.size() << endl;
     
     return dns_log_list;
 }
@@ -43,7 +62,7 @@ int main(int argc, char const *argv[]){
         exit(0);
     }
 
-    std::string file_name = std::string(argv[1]);
+    string file_name = string(argv[1]);
 
     process_dns_log(file_name);
     return 0;
