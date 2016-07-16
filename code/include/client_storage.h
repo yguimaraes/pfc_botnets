a10c2c5ef5b64046387b0b094aefa24007d041e2
Rocha Lima <jonasrla@gmail.com>
2016-07-15 22:36:55 -0300

    implements get_clients()

--- code/include/client_storage.h
+++ code/include/client_storage.h
@@ -1,15 +1,21 @@
 #pragma once
 
+#include <unordered_set>
+#include <string>
+#include <libpq-fe.h>
 
 class ClientStorage
 {
 public:
 	virtual void save() = 0;
+	virtual unordered_set<string> * get_clients() = 0;
 };
 
 class ClientStorageSql: public ClientStorage{
+	PGconn* m_connection;
 public:
 	ClientStorageSql();
 	~ClientStorageSql();
 	void save();
+	unordered_set<string> * get_clients();
 };
--- code/src/client_storage/client_storage.cpp
+++ code/src/client_storage/client_storage.cpp
@@ -1,15 +1,23 @@
-
-
 #include "client_storage.h"
 
 ClientStorageSql::ClientStorageSql(){
+using namespace std;
 
 }
 
 ClientStorageSql::~ClientStorageSql(){
-
+	PQfinish(m_connection);
 }
 
 void ClientStorageSql::save(){
 
+unordered_set<string> * ClientStorageSql::get_clients(){
+	unordered_set<string> * clients = new unordered_set<string>();
+	string sSQL = "SELECT client_ip FROM clients;";
+	PGresult * res = PQexec(m_connection, sSQL.c_str());
+	for (int i = 0; i < PQntuples(res); i++){
+		clients->insert(PQgetvalue(res, i, 0));
+	}
+	PQclear(res);
+	return clients;
 }
