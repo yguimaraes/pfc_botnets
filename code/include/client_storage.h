83d5956ba5d88006eab2146e0f991db52ab8de99
Rocha Lima <jonasrla@gmail.com>
2016-07-15 22:43:02 -0300

    implaments save(client info) function

--- code/include/client_storage.h
+++ code/include/client_storage.h
@@ -3,11 +3,14 @@
 #include <unordered_set>
 #include <string>
 #include <libpq-fe.h>
+#include "client_info.h"
+
+using namespace std;
 
 class ClientStorage
 {
 public:
-	virtual void save() = 0;
+	virtual void save(ClientInfo*) = 0;
 	virtual unordered_set<string> * get_clients() = 0;
 };
 
@@ -16,6 +19,6 @@ class ClientStorageSql: public ClientStorage{
 public:
 	ClientStorageSql();
 	~ClientStorageSql();
-	void save();
+	void save(ClientInfo*);
 	unordered_set<string> * get_clients();
 };
