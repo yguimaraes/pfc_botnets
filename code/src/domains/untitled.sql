db714206d9ffa951a3323599b08f72b0dd0e3e7e
Yago Guimaraes <yguimaraescoimbra@gmail.com>
2016-07-17 21:17:52 -0300

    calculate requisition degrees

--- code/src/domains/untitled.sql
+++
@@ -1,8 +0,0 @@
-UPDATE domains x
-SET requisition_degree = y.requisition_count
-FROM (
-	SELECT domains.domain, count(DISTINCT dns_queries.client_ip) as requisition_count
-	FROM domains INNER JOIN dns_queries ON domains.domain=dns_queries.domain 
-	GROUP BY domains.domain
-) AS y
-WHERE x.domain = y.domain;
