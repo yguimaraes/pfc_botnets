ec3762978547037d0c782360cf9490590362ef90
Rocha Lima <jonasrla@gmail.com>
2016-08-04 18:42:28 -0300

    smallest cluster reads config.json arguments

--- code/python/smallest_cluster.py
+++ code/python/smallest_cluster.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python
 from sklearn import cluster
 from utils import *
+import json
 
 def get_cluster_distribution(cluster_labels, n_clusters=8):
 	return [sum(i==cluster_labels) for i in range(n_clusters)]
 
-def main():
-	nc = 4
+def main(features=[], algorithm="KMeans", param=dict()):
+	nc = 1
+	(X,Y) = load(features)
 
-	(X,Y) = load()
-	# (X,Y) = shuffle_dataset(X, Y)
-	# (X_train, Y_train, X_test, Y_test) = separate_dataset(X, Y)
 	X = normalize(X)
-	mKmeans = cluster.KMeans(n_clusters=nc, 
-							 init='k-means++', 
-							 n_init=10, 
-							 max_iter=300, 
-							 tol=0.0001, 
-							 precompute_distances='auto', 
-							 verbose=0, 
-							 random_state=None, 
-							 copy_x=True, 
-							 n_jobs=1)
-	
-	cluster_labels = mKmeans.fit_predict(X)
-	dist = get_cluster_distribution(cluster_labels, n_clusters=nc)
-	index = dist.index(min(dist))
-	smallest = Y[cluster_labels==index]
-	print dist
-	for i in smallest:
-		print i
 
+	cluster_obj = getattr(cluster, algorithm)(**param)
+	cluster_labels = cluster_obj.fit_predict(X)
 
+	dist = get_cluster_distribution(cluster_labels, n_clusters=param["n_clusters"])
+	ord_dist = sorted(dist)
+	print ord_dist
+	for i in range(nc):
+		print "\n cluster %s" % i
+		index = dist.index(ord_dist[i])
+		smallest = Y[cluster_labels==index]
+		for i in smallest:
+			print i
 if __name__ == '__main__':
 	main()
+	args = json.load(file("config.json"))
+	main(**args)
 
