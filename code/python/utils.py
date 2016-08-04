aaffa4c35df5c0d1687b685bfe85becdeff6c090
Rocha Lima <jonasrla@gmail.com>
2016-08-04 18:35:29 -0300

    deletes separate_dataset

--- code/python/utils.py
+++ code/python/utils.py
@@ -23,32 +23,5 @@ def load(features):
 	return (dataset, ids)
 
 
-def separate_dataset(dataset, ids, rate=(0.8,0.2), validation=False):
-	
-	if sum(rate)!=1:
-		raise Exception("Rate argument must sum up to 1")
-	if (validation and len(rate) > 3) or ((not validation) and len(rate) > 2):
-		raise Exception("Too many rate arguments")
-	elif validation and len(rate) < 3:
-		raise Exception("You must set the validation size rate")
-	elif (not validation) and len(rate) < 2:
-		raise Exception("You must set the test size rate")
-
-	train_idx = int(rate[0]*ids.shape[0])
-	test_idx = int((rate[0] + rate[1]) * ids.shape[0])
-	
-	Y_train = ids[:train_idx]
-	Y_test = ids[train_idx:test_idx]
-	
-	X_train = dataset[:train_idx]
-	X_test = dataset[train_idx:test_idx]
-	if validation:
-		Y_val = ids[test_idx:]
-		X_val = dataset[test_idx:]
-		return (X_train, Y_train, X_test, Y_test, X_val, Y_val)
-
-	return (X_train, Y_train, X_test, Y_test)
-
-
 def normalize(X):
 	return (X-np.mean(X,axis=0))/np.std(X,axis=0)
